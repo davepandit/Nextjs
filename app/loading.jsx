9dab70b20afe82b796a17a81f269a7d94e76e6d9
Debajyoti Pandit <codeninja0812@gmail.com>
2024-06-26 20:53:50 +0530

    Add custom error and loading page

---
+++ app/loading.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+import {RotatingTriangles} from 'react-loader-spinner'
+
+const LoadingPage = () => {
+    return (
+        <div className='justify-center flex items-center h-screen'>
+            <RotatingTriangles
+            visible={true}
+            height="80"
+            width="80"
+            color="#4fa94d"
+            ariaLabel="rotating-triangles-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+        />
+        </div>
+    )
+}
+
+export default LoadingPage
