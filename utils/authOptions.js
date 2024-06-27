b11756a1f4c78ed7f1f60d7f7158d391a82514b5
Debajyoti Pandit <codeninja0812@gmail.com>
2024-06-28 01:06:52 +0530

    Successful login feature added

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
+import User from "@/models/user.models";
+import connectDB from "@/config/db";
+
 
 //auth options
 export const authOptions = {
@@ -20,15 +23,31 @@ export const authOptions = {
         //this function will run on succesfull sign in
         async signIn({ account, profile }) {
             //connect to the database
+            await connectDB()
             //check if the user exists
+            const userExists = await User.findOne({ email: profile.email });
             //if not add the user to the database
+            if (!userExists) {
+                // Truncate user name if too long
+                const username = profile.name.slice(0, 20);
+        
+                await User.create({
+                  email: profile.email,
+                  username,
+                  image: profile.picture,
+                });
+              }
             //return true to allow sign in
+            return true
         },
         //modifies the session object 
         async session({session}){
             //get user from the database
+            const user = await User.findOne({ email: session.user.email });
             //assign the user id to the session 
+            session.user.id = user._id.toString();
             //return session
+            return session;
         }
       },
 }
